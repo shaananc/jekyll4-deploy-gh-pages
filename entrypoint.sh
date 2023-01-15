291aa19ee6c3b36de567a0b8149c2fb5ad083093
Shaanan Cohney <shaananc@gmail.com>
2023-01-15 23:29:09 +1100

    hack for older libffi

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,8 @@ echo "Installing gems..."
 bundle config path vendor/bundle
 bundle config build.ffi --disable-system-libffi
 bundle install --jobs 4 --retry 3
+# hack for ffi gem to work on old debian runners
+gem pristine ffi --disable-system-libffi
 
 echo "Building Jekyll site..."
 
