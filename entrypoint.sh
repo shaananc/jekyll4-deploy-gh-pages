6baa9f713f0420c1faa85b81f1da61160d6c9fad
Shaanan Cohney <shaananc@gmail.com>
2023-02-06 00:32:42 +1100

    change pull to fetch

--- entrypoint.sh
+++ entrypoint.sh
@@ -123,7 +123,7 @@ function fetch_other_release() {
 publishdate=$(date +%m-%d-%Y)
 echo "Creating release for current branch"
 
-git pull origin main
+git fetch origin main
 
 # Define the API endpoint for creating a release
 NEW_BRANCH_NAME=$(git branch --show-current)
